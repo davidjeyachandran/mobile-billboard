6bf3cb1a0620f253432a1020a9f3a62027bf0231
David Jeyachandran <davidjeyachandran@gmail.com>
2019-03-12 16:14:30 -0500

    Updates

---
+++ precache-manifest.e2a329e170559028b6f43f6c0fa8fdbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "98cc4194abc8368db844",
+    "url": "/mobile-billboard/static/js/runtime~main.b1801f77.js"
+  },
+  {
+    "revision": "e560ebcae8167b48926d",
+    "url": "/mobile-billboard/static/js/main.5c2bb92b.chunk.js"
+  },
+  {
+    "revision": "782431a7a9fd8d41f546",
+    "url": "/mobile-billboard/static/js/2.8b2938bc.chunk.js"
+  },
+  {
+    "revision": "e560ebcae8167b48926d",
+    "url": "/mobile-billboard/static/css/main.0cca5111.chunk.css"
+  },
+  {
+    "revision": "8e6414cecf18343f6b2fdb9aeab33116",
+    "url": "/mobile-billboard/index.html"
+  }
+];
